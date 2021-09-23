bf4f799afc3a0da3d0aa110e64197d0266fa8fd2
An12854 <50544887+An12854@users.noreply.github.com>
2021-09-23 15:58:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -156,9 +156,6 @@ async def on_message(message):
   if message.content.startswith('~scp'):
     await message.channel.send('http://scp-sandbox-3.wikidot.com/ayymd')
 
-   if message.content.startswith('~pound MrAss'):
-    await message.channel.send('<@!682224399482748953> got pounded by a horse <a:succanimated:585943845846646785>')
-
   if message.content.startswith('~dcxd'):
     await message.channel.send('https://tenor.com/view/dead-chat-dead-discord-death-gif-18239566')
 
